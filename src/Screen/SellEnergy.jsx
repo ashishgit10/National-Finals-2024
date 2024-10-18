adf8effe320ef9419f1b4f72ecc0820ecc736a6e
ASHISH KUMAR SHARMA <ashishsharma.mail10@gmail.com>
2024-10-18 08:49:35 +0530

    Finally Done ❤

--- src/Screen/SellEnergy.jsx
+++ src/Screen/SellEnergy.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import web3 from '../web3';
+import web3 from '../web3.js';
 import listingcontractABI from '../abi/ListingContract.json';
 import Sidebar from '../Components/Sidebar';
 import WalletId from '../Components/WalletId';
